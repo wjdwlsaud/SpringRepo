cd4bd4deb2d33211b4286478353d955f5b84ff05
wjdwlsaud <wjdwlsaud_99@naver.com>
2022-07-29 13:13:56 +0900

    Spring : TestDTO : @Builder 추가 Spring : DTOTest : builder 사용해서 수정

--- src/main/java/com/korea/domain/TestDTO.java
+++ src/main/java/com/korea/domain/TestDTO.java
@@ -1,16 +1,20 @@
 package com.korea.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import sun.jvm.hotspot.oops.Mark;
 
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class TestDTO
 {
     private String name;
     private String age;
     private String addr;
+
 }
