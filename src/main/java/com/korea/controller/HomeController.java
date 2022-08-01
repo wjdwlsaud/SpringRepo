6157dca2e6c5e55ccb8a7c1762786a1fea7946d0
wjdwlsaud <wjdwlsaud_99@naver.com>
2022-08-01 09:25:14 +0900

    Spring : Log4j 추가

--- src/main/java/com/korea/controller/HomeController.java
+++ src/main/java/com/korea/controller/HomeController.java
@@ -1,11 +1,13 @@
 package com.korea.controller;
 
 import com.korea.domain.TestDTO;
+import lombok.extern.log4j.Log4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@Log4j
 public class HomeController
 {
 
